89864aa72b092bf80a0a1f0557c4b16d48b98970
sean <sean@wall.ninja>
2019-04-23 16:03:31 -0500

    Move guac-db build files to seperate directory

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+################################## EDIT THIS ##################################
 # PostgreSQL admin account:
 pg_user=postgres
 pg_password=guacamole
@@ -10,13 +11,22 @@ pg_password=guacamole
 guac_user=guacamole
 guac_password=guacamole
 
+# Change this to the IP address your reverse proxy, if you have one
+ip="127.0.0.1"
+###############################################################################
+
+# make a directory to build the postgres image
+mkdir guac-db
+
+# make a directory to build the guacamole image
+mkdir guacamole
 
 # generate the initdb.sql script
 sudo docker run --rm guacamole/guacamole \
-    /opt/guacamole/bin/initdb.sh --postgres > initdb.sql
+    /opt/guacamole/bin/initdb.sh --postgres > guac-db/initdb.sql
 
 # append guacamole user creation to initdb.sql
-cat <<EOT >> initdb.sql
+cat <<EOT >> guacd-db/initdb.sql
 
 
 CREATE USER $guac_user WITH PASSWORD '$guac_password';
@@ -24,11 +34,12 @@ GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO $guac_user;
 GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO $guac_user;
 EOT
 
+
 # create a Dockerfile which extends postgres with the initdb.sql script
 #
 # anything placed into the /docker-entrypoint-initdb.d/ directory will be run
 # automatically when the container starts
-cat <<EOT >> Dockerfile
+cat <<EOT >> guac-db/Dockerfile
 FROM postgres
 ENV POSTGRES_DB guacamole_db
 ENV POSTGRES_USER $pg_user
@@ -40,7 +51,7 @@ EOT
 sudo docker volume create guac-data
 
 # run the docker containers, and restart them after a reboot
-sudo docker build . --tag guac-pg
+sudo docker build guac-db --tag guac-pg
 sudo docker run --restart unless-stopped --name guac-pg -d guac-pg \
     -v guac-data:/var/lib/postgres/data
 sudo docker run --restart unless-stopped --name guacd -d guacamole/guacd
@@ -52,3 +63,23 @@ sudo docker run --restart unless-stopped --name guacamole \
     -e POSTGRES_PASSWORD=guacamole \
     -e POSTGRES_USER=guacamole \
     -d -p 8080:8080 guacamole/guacamole
+
+
+# Add this!
+# ip="192.168.1.12"
+
+# cat <<EOF > tmp
+# <Valve className="org.apache.catalina.valves.RemoteIpValve"
+#                internalProxies="$ip"
+#                remoteIpHeader="x-forwarded-for"
+#                remoteIpProxiesHeader="x-forwarded-by"
+#                protocolHeader="x-forwarded-proto" />
+
+# EOF
+
+# # find the line number of the "</Host>" line, and return only the line number
+# line=`grep -n '</Host>' server | cut -f1 -d:`
+# line=`expr $line - 1`
+
+# sed "${line}r tmp" server
+# rm tmp
